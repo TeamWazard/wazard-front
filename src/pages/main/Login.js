15534e2668962f10bff93d07c32a59902554b0b9
jjjuyoa <jjjuyoa@gmail.com>
2023-03-30 16:22:05 +0900

    fix: 로그인 화면 아이콘 추가

--- src/pages/main/Login.js
+++ src/pages/main/Login.js
@@ -1,5 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { Link } from "react-router-dom";
+import userIcon from "../../imgs/userIcon.svg";
+import passwordIcon from "../../imgs/passwordIcon.svg";
 
 const User = {
   email: "jjjuyoa@gmail.com",
@@ -43,39 +45,50 @@ export default function Login() {
     }
   };
   const onClickConfirmButton = () => {
-    if (email === User.email && password === User.password) {
+    if (email === "") {
+      emailInput.current.focus();
+    } else if (email === User.email && password === User.password) {
       alert("로그인에 성공했습니다.");
     } else {
       alert("등록되지 않은 회원입니다.");
     }
   };
 
+  const emailInput = useRef();
+  const passwordInput = useRef();
+
   return (
     <div className="all">
       <div className="page">
         <div className="titleWrap">Wazard</div>
         <div className="contentWrap">
-          <div className="inputContainer">
-            <div className="inputWrap">
-              <input
-                className="input"
-                type="text"
-                placeholder="test@email.com"
-                value={email}
-                onChange={handleEmail}
-              />
-            </div>
-            <div className="inputWrap" style={{ marginTop: "40px" }}>
-              <input
-                className="input"
-                type="password"
-                placeholder="영문, 숫자, 특수문자 포함 8자 이상"
-                value={password}
-                onChange={handlePw}
-              />
-            </div>
-            <h3 className="hihi">Forgot your Email or Password ?</h3>
-            <h3>Not a Member?</h3>
+          <div className="subLogin">
+            <img src={userIcon} alt="" />
+            <h2>이메일</h2>
+          </div>
+          <div className="inputWrap">
+            <input
+              ref={emailInput}
+              className="input"
+              type="text"
+              // placeholder="test@email.com"
+              value={email}
+              onChange={handleEmail}
+            />
+          </div>
+          <div className="subLogin">
+            <img src={passwordIcon} alt="" />
+            <h2>비밀번호</h2>
+          </div>
+          <div className="inputWrap">
+            <input
+              ref={passwordInput}
+              className="input"
+              type="password"
+              // placeholder="영문, 숫자, 특수문자 포함 8자 이상"
+              value={password}
+              onChange={handlePw}
+            />
           </div>
           <div>
             <button
@@ -83,21 +96,18 @@ export default function Login() {
               disabled={notAllow}
               className="loginButton"
             >
-              ENTER
+              LOGIN
             </button>
           </div>
-        </div>
-        {/* 버튼 */}
-        <div className="signUpButtonContainer">
-          <button
-            onClick={onClickConfirmButton}
-            disabled={notAllow}
-            className="signUpButton"
-          >
-            sign up
-          </button>
+          <div className="signUpButtonContainer">
+            <button className="signUpButton">
+              <Link to="/signupchoose">SIGN UP</Link>
+            </button>
+          </div>
+          <h3 className="hihi">Forgot your Email or Password ?</h3>
         </div>
       </div>
+      {/* 버튼 */}
     </div>
   );
 }
