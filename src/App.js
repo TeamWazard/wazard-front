31ce23ab4c4a37072770a09eef5699302b435f06
jjjuyoa <jjjuyoa@gmail.com>
2023-04-15 09:30:40 +0900

    feat: 테스트용 page 추가(추후 삭제)

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import Login from "./pages/main/Login";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SignUpChoose from "./pages/main/SignUpChoose";
 import ButtonChoose from "./pages/main/ButtonChoose";
+import CeoMain from "./pages/main/CeoMain";
+import CeoMain2 from "./pages/main/CeoMain2";
+import CeoMain3 from "./pages/main/CeoMain3";
+import CeoMain4 from "./pages/main/CeoMain4";
 
 function App() {
   return (
@@ -13,6 +17,10 @@ function App() {
           <Route exact path="/" element={<Login />} />
           <Route path="/signup" element={<SignUpChoose />} />
           <Route path="/buttonChoose" element={<ButtonChoose />} />
+          <Route path="/ceomain" element={<CeoMain />} />
+          <Route path="/ceomain2" element={<CeoMain2 />} />
+          <Route path="/ceomain3" element={<CeoMain3 />} />
+          <Route path="/ceomain4" element={<CeoMain4 />} />
         </Routes>
       </div>
     </BrowserRouter>
