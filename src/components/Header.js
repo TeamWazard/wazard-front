41361f2ec5423c936a93bd3572f89d5da9d82628
jjjuyoa <jjjuyoa@gmail.com>
2023-04-15 09:31:53 +0900

    feat: Header 컴포넌트 제작

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import "../App.scss";
+
+const Header = () => {
+  return (
+    <div className="topbar">
+      <div className="topTitle">Wazard</div>
+      <div className="topMyPage">ㅁㅁㅁㅁㅁ</div>
+    </div>
+  );
+};
+
+export default Header;
