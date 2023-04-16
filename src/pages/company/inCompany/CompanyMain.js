d13f141415bd40c7dd00fb0b3f8de23e46737f02
seoyoung7623 <seoyoung7623@naver.com>
2023-04-17 03:12:57 +0900

    feat: 지정날짜 알바생 목록 조회

--- src/pages/company/inCompany/CompanyMain.js
+++ src/pages/company/inCompany/CompanyMain.js
@@ -16,7 +16,20 @@ const CompanyMain = (props) => {
     state.companies.find((company) => company.company_id === parseInt(id))
   );
   const [value, onChange] = useState(new Date());
-
+  const members = [
+    {
+      user_id: 0,
+      user_name: "윤서영",
+      start_time: "12:58:29",
+      end_time: "18:01:02",
+    },
+    {
+      user_id: 1,
+      user_name: "김정환",
+      start_time: "18:00:02",
+      end_time: "23:00:30",
+    },
+  ];
   return (
     <div className="main">
       <Header />
@@ -30,7 +43,31 @@ const CompanyMain = (props) => {
               <label>날에 근무한 알바생</label>
             </div>
 
-            <div className="memberList"></div>
+            <div className="memberList">
+              <div className="memberList-view">
+                {members.map((member, index) => (
+                  <div className="member">
+                    <div className="number" key={index}>
+                      {index + 1} )
+                    </div>
+                    <div className="name">
+                      <label>이름:</label>
+                      {member.user_name}
+                    </div>
+                    <div className="time">
+                      <div className="startTime">
+                        <label>출근 시간:</label>
+                        {member.start_time}
+                      </div>
+                      <div className="endTime">
+                        <label>퇴근 시간:</label>
+                        {member.end_time}
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
           </div>
         </div>
       </div>
