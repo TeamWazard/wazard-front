06469781b658c8e5d016ff913ff8a00e3ce7fed0
seoyoung7623 <seoyoung7623@naver.com>
2023-05-19 20:20:58 +0900

    feat:알바생 초대코드 입력페이지 추가

--- src/pages/alba/AlbaListPage.js
+++ src/pages/alba/AlbaListPage.js
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import HeaderAlba from "components/HeaderAlba";
 import CompanyListView from "components/company/CompanyListView";
 
@@ -5,6 +6,7 @@ import "../../style/company/company.scss";
 import "../../style/alba/albaCompanyList.scss";
 
 function AlbaList() {
+  const navigate = useNavigate();
   return (
     <div className="company_list_page">
       <HeaderAlba />
@@ -15,7 +17,13 @@ function AlbaList() {
 
         <CompanyListView />
         <div className="alba-plus_company">
-          <button>초대코드 입력하기</button>
+          <button
+            onClick={() => {
+              navigate("/alba_invited");
+            }}
+          >
+            초대코드 입력하기
+          </button>
         </div>
       </div>
     </div>
