8793bdb48d66e4b59d794d4dc593c5fc343c6654
seoyoung7623 <seoyoung7623@naver.com>
2023-05-30 01:17:38 +0900

    feat: 초대코드 & 대기자 명단 화면

--- src/pages/company/inCompany/CompanyInvite.js
+++ src/pages/company/inCompany/CompanyInvite.js
@@ -1,13 +1,31 @@
 import Header from "components/Header";
 import { useNavigate, useParams } from "react-router-dom";
 import LeftMenuCeo from "components/LeftMenuCeo";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import "../../../style/company/company.scss";
 
 const CompanyInvite = () => {
   const [userEmail, setUserEmail] = useState("");
   const [isValid, setIsValid] = useState(false);
+  const [isState, setIsState] = useState(true);
+  const invite_user = [
+    {
+      id: 0,
+      name: "김민규",
+      email: "hello123@naver.com",
+      code: "24DW4Y89",
+      state: "대기",
+    },
+    {
+      id: 1,
+      name: "윤서영",
+      email: "seoyoung7623@naver.com",
+      code: "DE32S5DU",
+      state: "거절",
+    },
+  ];
+  useEffect(() => {}, [invite_user]);
 
   const validateEmail = (input) => {
     const regex =
@@ -58,6 +76,46 @@ const CompanyInvite = () => {
               <div className="btn-wrapper">
                 <button onClick={handlesubmit}>초대하기</button>
               </div>
+            </div>
+          </div>
+          <div className="waitlist-container">
+            <div className="title">
+              <h2>초대 대기 목록</h2>
+            </div>
+            <div className="waitlist-wrapper">
+              {invite_user.map((user) => (
+                <div className="waitlist-main">
+                  <div className="waitlist-set">
+                    <div className="waitlist-things">
+                      <div className="name waitlist-one">
+                        <label className="value">이름</label>
+                        <label>{user.name}</label>
+                      </div>
+                      <div className="email waitlist-one">
+                        <label className="value">이메일</label>
+                        <label>{user.email}</label>
+                      </div>
+                      <div className="inviteCode waitlist-one">
+                        <label className="value">초대코드</label>
+                        <label>{user.code}</label>
+                      </div>
+                    </div>
+                    <div className="invite-state">
+                      {user.state === "대기" && (
+                        <label className="wait state">대기</label>
+                      )}
+                      {user.state === "거절" && (
+                        <label className="disagee state">거절</label>
+                      )}
+                    </div>
+                  </div>
+                  {user.state == "거절" && (
+                    <div className="contract-wrapper">
+                      <button>계약정보 수정</button>
+                    </div>
+                  )}
+                </div>
+              ))}
             </div>
           </div>
         </div>
